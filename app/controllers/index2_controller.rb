efb9dcac2c0c875d7ec06789321297a1ba91d691
Rainiero Rojas <rainiero.rv@hotmail.com>
2017-01-23 20:52:29 -0300

    Intial Commit

---
+++ app/controllers/index2_controller.rb
@@ -0,0 +1,7 @@
+class Index2Controller < ApplicationController
+  def indexx
+    @variable1 = "Variable dinámica 1"
+    @var2 = "Var dinamica 2 (ingresadas en controlador index2)"
+  end
+end
+#se pueden agregar + funciones aparte de indexx
