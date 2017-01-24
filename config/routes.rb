54e96cea92506f05e680fde6b227ff096114b07b
Rainiero Rojas <rainiero.rv@hotmail.com>
2017-01-23 21:15:00 -0300

    Corregido para que funcione

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'index2/indexx'
 
-  get 'index/index', to: 'index#index'
+  get 'index/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
